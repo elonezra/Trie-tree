870eca39d7a02483f54419f169c1fade47025de4
yarden7696 <yarden7696@gmail.com>
2020-01-08 13:49:22 -0500

    almost done! need to free memory and read from file :)

---
+++ Ex4.h
@@ -0,0 +1,32 @@
+#include <stdbool.h>
+
+#ifndef _TRIE_H_
+#define _TRIE_H_
+
+
+#define WORD (int)30
+#define LINE (int)255
+#define CHAR_2_INDEX(letter) ((int)letter-(int)'a'); //return asci's value of letter
+#define NUM_LETTERS (int)26
+#define ARRAY_SIZE(a) (sizeof(a)/sizeof(a[0])) 
+
+typedef enum boolean : bool{fls=0, tru=1};
+
+
+typedef struct Node node;
+
+
+node *getNode();
+void insert(node *root, const char *word) 
+{boolean search(node *root, const char *word);}
+int wordAmount(node *root, const char *word);
+void display( node* root, char str[], int level);
+void displayR( node* root, char str[], int level);
+
+
+
+#endif
+
+
+
+
